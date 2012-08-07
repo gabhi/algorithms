ac8bcb20a5ff9ea4fff09f8ad3a51aca55c3c650
Abhijit Gaikwad <gaikwad.abhijit@gmail.com>
2012-08-06 22:29:28 -0700

    Adding initial files

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Abhijit Gaikwad <gaikwad.abhijit@gmail.com>
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
