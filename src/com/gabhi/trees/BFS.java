9ac8b6629c949556b7215d43ad03bc27201a6685
Abhijit Gaikwad <gaikwad.abhijit@gmail.com>
2012-08-09 00:36:14 -0700

    BFS

---
+++ src/com/gabhi/trees/BFS.java
@@ -0,0 +1,44 @@
+/*
+ * The MIT License
+ *
+ * Copyright 2012 Abhijit Gaikwad <gaikwad.abhijit@gmail.com>.
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+package com.gabhi.trees;
+
+/**
+ *
+ * @author Abhijit Gaikwad <gaikwad.abhijit@gmail.com> 
+ * visit http://gabhi.com 
+ */
+public class BFS {
+//Breadth-first search
+
+    
+//    The algorithm uses a queue data structure to store intermediate results as it traverses the graph, as follows:
+//
+//Enqueue the root node
+//Dequeue a node and examine it
+//If the element sought is found in this node, quit the search and return a result.
+//Otherwise enqueue any successors (the direct child nodes) that have not yet been discovered.
+//If the queue is empty, every node on the graph has been examined – quit the search and return "not found".
+//If the queue is not empty, repeat from Step 2.
+}
