2d8db063178dbb9a1dd47e312c58c62e63af36db
Abhijit Gaikwad <gaikwad.abhijit@gmail.com>
2012-08-11 00:09:37 -0700

    Fibonacci

--- src/com/gabhi/array/ArrayPermutations.java
+++ src/com/gabhi/array/ArrayPermutations.java
@@ -32,29 +32,31 @@ import java.util.List;
  */
 public class ArrayPermutations {
 
-    // print N! permutation of the characters of the string s (in order)
-    public static void stringPermutations(String input) {
-        stringPermutations("", input);
+  // print N! permutation of the characters of the string s (in order)
+  public static void stringPermutations(String input) {
+    stringPermutations("", input);
+  }
+
+  private static void stringPermutations(String prefix, String input) {
+    int N = input.length();
+
+    System.out.println(prefix);
+    if (N == 0) {
+      //Do nothing
+    } else {
+      for (int i = 0; i < N; i++) {
+        stringPermutations(
+                prefix + input.charAt(i),
+                input.substring(0, i) + input.substring(i + 1, N));
+      }
     }
 
-    private static void stringPermutations(String prefix, String input) {
-        int N = input.length();
+  }
 
-        System.out.println(prefix);
-        if (N == 0) {
-            //Do nothing
-        } else {
-            for (int i = 0; i < N; i++) {
-                stringPermutations(prefix + input.charAt(i), input.substring(0, i) + input.substring(i + 1, N));
-            }
-        }
+  public static void main(String[] args) {
 
-    }
-
-    public static void main(String[] args) {
-
-        String elements = "abc";
-        stringPermutations(elements);
-        System.out.println();
-    }
+    String elements = "abc";
+    stringPermutations(elements);
+    System.out.println();
+  }
 }
